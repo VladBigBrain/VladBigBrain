3b1a3516487d813b1fce5c0fdb1c3e46ec4b00e4
Barnards <barnards@student.21-school.ru>
2023-09-23 17:51:37 +0700

    back prop before qt

--- MLP/tests/model_tests.cc
+++ MLP/tests/model_tests.cc
@@ -35,18 +35,17 @@ TEST(TestGraph, GraphFeedForward) {
 }
 
 TEST(TestGraph, GraphBack) {
-  // s21::NeuralNetwork nn(2, 120, 784);
-  // auto inputs = Eigen::VectorXd::Random(784);
-  // auto result = nn.FeedForward(inputs);
+  s21::GraphPerceptrone nn(2, 120, 784);
+  auto inputs = Eigen::VectorXd::Random(784);
+  auto result = nn.FeedForward(inputs);
 
-  // auto fnc = [&](const Eigen::VectorXd& inputs, int target) ->
-  // Eigen::VectorXd {
-  //   Eigen::VectorXd targetVector(inputs.size());
-  //   targetVector.setZero();
-  //   targetVector(target) = 1.0;
-  //   return targetVector;
-  // };
+  auto fnc = [&](const Eigen::VectorXd& inputs, int target) -> Eigen::VectorXd {
+    Eigen::VectorXd targetVector(inputs.size());
+    targetVector.setZero();
+    targetVector(target) = 1.0;
+    return targetVector;
+  };
 
-  // nn.BackPropagation(result, fnc(result, 13), 0.1);
-  // ASSERT_EQ(result.size(), 26);
+  nn.BackPropagation(result, fnc(result, 13), 0.1);
+  ASSERT_EQ(result.size(), 26);
 }
